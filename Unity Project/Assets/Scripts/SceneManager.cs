fe24297cae86add251d4a5c12baa0948b9bc0f97
SweeneyPro <sweeney.pro@hotmail.co.uk>
2018-06-13 12:50:23 +0100

    Setup Scene Transitions + Hooked Up Art

---
+++ Unity Project/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MainMenuScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void LevelTypeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameTypeScene");
+    }
+
+    public void BlitzScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Blitz");
+    }
+
+    public void PuzzleScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Puzzle");
+    }
+
+    public void SettingsScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SettingsScene");
+    }
+
+    public void StatsScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("StatsScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        
+    }
+}
