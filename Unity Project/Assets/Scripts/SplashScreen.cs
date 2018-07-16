d48721608218b8c07e236ff0158d342554f8525c
SweeneyPro <sweeney.pro@hotmail.co.uk>
2018-07-16 17:50:35 +0100

    Tweaks

---
+++ Unity Project/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(SwitchScene());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SwitchScene ()
+    {
+        yield return new WaitForSeconds(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene-Jamie");
+    }
+}
