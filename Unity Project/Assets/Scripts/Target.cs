5f69a426d57ab78930a159813f7bb3d60fde584c
Michael Sweeney <sweeney.pro@hotmail.co.uk>
2018-07-20 20:50:32 +0100

    Added targets for story mode

---
+++ Unity Project/Assets/Scripts/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Target : MonoBehaviour {
+
+	public Level levelobjects;
+
+	public Text targetone;
+	public Text targettwo;
+	public Text targetthree;
+
+	// Use this for initialization
+	void Start () {
+
+		targetone.text = levelobjects.score1Star.ToString ();
+		targettwo.text = levelobjects.score2Star.ToString ();
+		targetthree.text = levelobjects.score3Star.ToString ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
