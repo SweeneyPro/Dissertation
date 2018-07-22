da54e51582c17b58f4b16ea1075a189bcd6f8f7c
Michael Sweeney <sweeney.pro@hotmail.co.uk>
2018-07-22 19:51:05 +0100

    Android Build

---
+++ Unity Project/Assets/Scripts/Analytics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+
+public class Analytics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Analytics.Analytics.Transaction ("test", 1, "mon");
+		UnityEngine.Analytics.Analytics.CustomEvent ("DATA", new Vector3(Random.Range(20,30), Random.Range(50000, 100000), Random.Range(1,10)));
+		UnityEngine.Analytics.Analytics.CustomEvent ("TEST");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity Project/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			ScreenCapture.CaptureScreenshot ("SS");
+		}
+	}
+
+
+}
